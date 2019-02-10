91e42f72360c393e18149c3fe1af8be4582f84e1
Coco <cocojoker3@gmail.com>
2019-02-10 13:05:41 +1000

    navigation and favicons

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,1 +1,11 @@
 cssVars();
+
+// open mobile nav
+
+$('.nav-prompt').click(function () {
+    if ($('.nav-inner').attr('aria-expanded') == 'false') {
+        $('.nav-inner').attr('aria-expanded', 'true');
+    } else {
+        $('.nav-inner').attr('aria-expanded', 'false');
+    }
+})
